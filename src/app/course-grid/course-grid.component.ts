f6ed6b6cbc6247f84548a550002b9cab227faacb
Luke Horgan <horgan.l@husky.neu.edu>
2018-06-20 22:45:04 -0400

    enroll in a class

--- src/app/course-grid/course-grid.component.ts
+++ src/app/course-grid/course-grid.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {CourseServiceClient} from "../services/course.service.client";
 import {Course} from "../models/coruse.model.client";
+import {SectionServiceClient} from "../services/section.service.client";
 
 @Component({
   selector: 'app-course-grid',
@@ -9,13 +10,29 @@ import {Course} from "../models/coruse.model.client";
 })
 export class CourseGridComponent implements OnInit {
 
-  constructor(private service: CourseServiceClient) { }
+  constructor(private service: CourseServiceClient,
+              private sectionService: SectionServiceClient) { }
 
   courses: Course[] = [];
+  enrolledIn = new Set();
 
   ngOnInit() {
+    /*this.service.findAllCourses()
+      .then(courses => this.courses = courses);*/
     this.service.findAllCourses()
-      .then(courses => this.courses = courses);
+      .then(courses => {
+        return this.sectionService.findSectionsForStudent()
+                                  .then(sections => {
+                                    console.log(sections);
+                                    sections.map(section => {
+                                      console.log(section.enrollment.section.courseId);
+                                      this.enrolledIn.add(section.enrollment.section.courseId);
+                                    });
+                                    this.courses = courses.sort((a, b) => {
+                                      return this.enrolledIn.has(a.id);
+                                    });
+                                  })
+      });
   }
 
 }
